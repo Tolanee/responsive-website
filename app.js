4cbc6aa4043eb9a2cbd8c0096b79889b7f28f386
Tolanee <68304958+Tolanee@users.noreply.github.com>
2021-04-19 23:25:27 +0100

    responsive nav bar

---
+++ app.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('nav-links li');
+burger.addEventListener('click', ()=>{
+  navLinks.classList.toggle('open');
+})
